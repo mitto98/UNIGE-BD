1e8c9bbcd96b5264284a4f005d89e2ab76515a70
Mattia Dapino <mattia.dapino@gmail.com>
2020-04-25 15:19:26 +0200

    Unitificati script creazione procedure e funzione

--- 01-Creazione_trigger_e_procedure.sql
+++ 01-Creazione_trigger_e_procedure.sql
@@ -1,3 +1,7 @@
+------------------------------
+-- Trigger vincoli di stato --
+------------------------------
+
 -- Trigger limite tempo inserimetno
 -- Una prenotazione puo essere fatta solo entro 15 minuti dal ritiro
 CREATE OR REPLACE FUNCTION check_limite_prenotazione()
@@ -19,13 +23,12 @@ CREATE TRIGGER check_limite_prenotazione
     FOR EACH ROW
 EXECUTE PROCEDURE check_limite_prenotazione();
 
-
 -- Log modifica prenotazione
 CREATE OR REPLACE FUNCTION log_cambiamento_prenotazione()
     RETURNS TRIGGER AS
 $log_cambiamento_prenotazione$
 BEGIN
-    INSERT INTO modifica_prenotazione(numero_prenotazione, data_ora_rinuncia, nuova_data_ora_inizio, nuova_data_ora_rest)
+    INSERT INTO modifica_prenotazione(numero_prenotazione, data_ora_rinuncia, nuova_data_ora_inizio, nuova_data_ora_fine)
     VALUES (OLD.numero_prenotazione, now()::timestamp, OLD.data_ora_inizio, OLD.data_ora_fine);
     RETURN NEW;
 END;
@@ -36,7 +39,6 @@ CREATE TRIGGER log_cambiamento_prenotazione
     ON prenotazione
     FOR EACH ROW
 EXECUTE PROCEDURE log_cambiamento_prenotazione();
-
 
 -- Controllo coerenza date modifica prenotazione
 CREATE OR REPLACE FUNCTION check_coerenza_date_prenotazione()
@@ -49,7 +51,7 @@ BEGIN
     INTO bData
     FROM prenotazione
     WHERE prenotazione.numero_prenotazione = new.numero_prenotazione;
-    IF bData > now()::timestamp AND new.nuova_data_ora_inizio < new.nuova_data_ora_rest
+    IF bData > now()::timestamp AND new.nuova_data_ora_inizio < new.nuova_data_ora_inizio
     THEN
         new.data_ora_rinuncia = now()::timestamp;
         RETURN NEW;
@@ -84,3 +86,56 @@ CREATE TRIGGER check_validita_carta
     ON carta
     FOR EACH ROW
 EXECUTE PROCEDURE check_validita_carta();
+
+
+-------------------------------
+-- Procedure calcolo fattura --
+-------------------------------
+
+-- Tariffa oraria
+CREATE OR REPLACE FUNCTION tariffa_oraria(durata numeric, costo_orario numeric, costo_km numeric, chilometri numeric)
+    RETURNS numeric AS
+$$
+BEGIN
+    RETURN (costo_orario * durata) + (costo_km * chilometri);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Tariffa giornaliera
+CREATE OR REPLACE FUNCTION tariffa_day(giorni numeric, costo_day numeric, costo_km numeric, chilometri numeric)
+    RETURNS numeric AS
+$$
+BEGIN
+    RETURN (costo_day * giorni) + (costo_km * chilometri);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Tariffa settimanale
+CREATE OR REPLACE FUNCTION tariffa_week(giorni numeric, costo_week numeric, costo_day numeric, costo_km numeric,
+                                        chilometri numeric)
+    RETURNS Numeric AS
+$$
+BEGIN
+    RETURN ROUND(giorni / 7) * costo_week + tariffa_day(MOD(giorni, 7), costo_day, costo_km, chilometri);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Tariffa conveniente
+CREATE OR REPLACE FUNCTION tariffa_conveniente(durata numeric, costo_orario numeric, costo_day numeric,
+                                               costo_week numeric, costo_km numeric, chilometri numeric)
+    RETURNS numeric AS
+$$
+BEGIN
+    IF durata < 24
+    THEN
+        RETURN tariffa_oraria(durata, costo_orario, costo_km, chilometri);
+    ELSE
+        IF (durata / 24) < 7
+        THEN
+            RETURN tariffa_day(durata / 24, costo_day, costo_km, chilometri);
+        ELSE
+            RETURN tariffa_week(durata / 24, costo_week, costo_day, costo_km, chilometri);
+        END IF;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
